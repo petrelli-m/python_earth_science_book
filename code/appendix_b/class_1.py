f2e26b97c09ed96cabe220d0b131850e4cd8e079
Maurizio Petrelli <maurizio@Maurizios-MacBook-Pro.local>
2021-04-16 12:32:58 +0200

    code upload

--- code/appendix_b/class_1.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-class Circle:
-    # Attributes an methods here
-
-    
--- code/appendix_b/class_2.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-class Circle:
-
-    def __init__(self, radius):
-        self.radius = radius
-
-
-    
